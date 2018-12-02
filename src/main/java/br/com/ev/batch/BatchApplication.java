0b9d5a1e9ed95163409f14d10e462e020c536584
Brendon <freitas.brendon@gmail.com>
2018-12-02 19:57:51 -0200

    Two jobs execution

--- src/main/java/br/com/ev/batch/BatchApplication.java
+++ src/main/java/br/com/ev/batch/BatchApplication.java
@@ -1,12 +1,61 @@
 package br.com.ev.batch;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameter;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BatchApplication {
+public class BatchApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BatchApplication.class, args);
+	}
+	
+	@Autowired
+	private JobLauncher jobLauncher;
+	
+	@Autowired
+	@Qualifier("job1")
+	private Job job1;
+	
+	@Autowired
+	@Qualifier("job2")
+	private Job job2;
+	
+	private int x = 2;
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		if(x == 1) {
+			
+			Map<String, JobParameter> confMap = new HashMap<>();
+		    confMap.put("time", new JobParameter(System.currentTimeMillis()));
+		    JobParameters jobParameters = new JobParameters(confMap);
+			
+			jobLauncher.run(job1, jobParameters);
+			
+		}else {
+			
+			Map<String, JobParameter> confMap = new HashMap<>();
+		    confMap.put("time", new JobParameter(System.currentTimeMillis()));
+		    JobParameters jobParameters = new JobParameters(confMap);
+			
+			jobLauncher.run(job2, jobParameters);
+			
+		}
+		
+		
+		
+		
 	}
 }
--- src/main/java/br/com/ev/batch/configurations/BatchConfiguration.java
+++ src/main/java/br/com/ev/batch/configurations/BatchConfiguration.java
@@ -1,7 +1,9 @@
 package br.com.ev.batch.configurations;
 
 import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
 import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.job.builder.FlowBuilder;
@@ -20,6 +22,7 @@ import org.springframework.core.task.SimpleAsyncTaskExecutor;
  *
  */
 @Configuration
+@EnableBatchProcessing
 public class BatchConfiguration {
 	
 	@Autowired
@@ -31,6 +34,15 @@ public class BatchConfiguration {
 	@Bean
 	public Tasklet tasklet() {
 		return new CountingTasklet();
+	}
+	
+	@Bean
+	public Step step4() {
+		return stepBuilderFactory.get("step1")
+				.tasklet((contribution, chunkContext) -> {
+					System.out.println(">> Step 4 from job 2!!!");
+					return RepeatStatus.FINISHED;
+				}).build();
 	}
 	
 	@Bean
@@ -51,12 +63,20 @@ public class BatchConfiguration {
 				.build();
 	}
 	
-	@Bean
-	public Job job() {
-		return jobBuilderFactory.get("job")
+	
+	@Bean(name="job1")
+	public Job job1() {
+		return jobBuilderFactory.get("job1")
 				.start(flow1())
 				.split(new SimpleAsyncTaskExecutor()).add(flow2())
 				.end()
+				.build();
+	}
+	
+	@Bean(name="job2")
+	public Job job2() {
+		return jobBuilderFactory.get("job2")
+				.start(step4())
 				.build();
 	}
 	
