6b8125054a18531c7f889697fe84c396b0890dd2
Evelyn Vieira <vieira.evelyn05@gmail.com>
2019-01-29 15:06:39 -0200

    Job Flow - Nested Jobs

---
+++ src/main/java/br/com/ev/batch/configurations/ChildJobConfiguration.java
@@ -0,0 +1,42 @@
+package br.com.ev.batch.configurations;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 
+ * @author Evelyn
+ *
+ */
+@Configuration
+public class ChildJobConfiguration {
+	
+	@Autowired
+	private JobBuilderFactory jobBuilderFactory;
+	
+	@Autowired
+	private StepBuilderFactory stepBuilderFactory;
+	
+	@Bean
+	public Step step1a() {
+		return stepBuilderFactory.get("step1a")
+				.tasklet((contribution, chunckContext) -> {
+						System.out.println("\t>>This is step 1a");
+						return RepeatStatus.FINISHED;
+				}).build();
+	}
+	
+	@Bean
+	public Job childJob() {
+		return jobBuilderFactory.get("childJob")
+				.start(step1a())
+				.build();
+	}
+
+}
